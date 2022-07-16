01ea939cc1f49f902e1d8510c600f1c956bc8a93
Callan <cerveauxallanjean@gmail.com>
2022-07-16 19:45:49 +0200

    Add possibility to choose phaser version

--- src/commands/CreateProject.ts
+++ src/commands/CreateProject.ts
@@ -5,6 +5,7 @@ import { Tea } from 'tea-parser'
 import { __dirname } from '../utils'
 import generate from 'project-name-generator'
 import prompts from 'prompts'
+import { spawn } from 'child_process'
 
 function CreateProject() {
   const cmd = new Command('create')
@@ -17,8 +18,9 @@ function CreateProject() {
         'empty'
       ])
     )
+    .addOption(new Option('-pv, --phaser <phaser_version>', 'choose phaser version :').choices(['phaser-ce', 'phaser']))
     .action(async (project_name, option) => {
-      let { template } = option
+      let { template, phaser_version } = option
       if (!project_name) {
         project_name = (
           await prompts({
@@ -38,13 +40,39 @@ function CreateProject() {
             choices: [
               { title: 'Javascript', value: 'javascript' },
               { title: 'Typescript', value: 'typescript' },
-              { title: 'Empty', value: 'empty' }
+              { title: 'Empty', value: 'empty', description: 'without all scripts' }
             ],
             initial: 0
           })
         ).template
       }
-      Tea(join(__dirname, `../src/templates/${template}`), resolve(project_name), { project_name })
+      await Tea(join(__dirname, `../src/templates/${template}`), resolve(project_name), { project_name })
+      if (!phaser_version) {
+        phaser_version = (
+          await prompts({
+            type: 'select',
+            name: 'phaser_version',
+            message: 'Pick a phaser version',
+            choices: [
+              { title: 'Phaser 3', value: 'phaser' },
+              { title: 'Phaser CE', value: 'phaser-ce' }
+            ],
+            initial: 0
+          })
+        ).phaser_version
+        const install_phaser = await spawn(`npm`, ['install', phaser_version], {
+          stdio: [0, 1, 2],
+          cwd: resolve(project_name)
+        })
+        install_phaser.stdout?.on('data', data => console.log(data))
+        install_phaser.stderr?.on('data', data => console.log(data))
+        install_phaser.on('error', error => console.log(error))
+
+        const install = await spawn(`npm`, ['install'], { stdio: [0, 1, 2], cwd: resolve(project_name) })
+        install.stdout?.on('data', data => console.log(data))
+        install.stderr?.on('data', data => console.log(data))
+        install.on('error', error => console.log(error))
+      }
     })
   return cmd
 }
