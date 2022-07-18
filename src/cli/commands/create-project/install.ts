6cf5b2a23227c4473312f3b38b85b1dfc2b32805
Callan <cerveauxallanjean@gmail.com>
2022-07-18 17:22:17 +0200

    add command checker

--- src/cli/commands/create-project/install.ts
+++ src/cli/commands/create-project/install.ts
@@ -1,13 +1,14 @@
+import { commandExist } from '../../../utils'
 import { existsSync } from 'fs'
 import { resolve } from 'path'
 import { spawn } from 'child_process'
 
 async function install(package_manager: string, phaser_version: string, project_name: string) {
-  if (!existsSync(resolve(project_name)))
+  if (!existsSync(project_name) && (await commandExist(package_manager)))
     console.log(`Install package automatically has failed, try run '${package_manager} install' manually`)
   const install_arg = package_manager === 'yarn' ? 'add' : 'install'
 
-  const install = await spawn(package_manager, [install_arg], { stdio: [0, 1, 2], cwd: resolve(project_name) })
+  const install = await spawn(package_manager, ['install'], { stdio: [0, 1, 2], cwd: resolve(project_name) })
   install.stdout?.on('data', data => console.log(data))
   install.stderr?.on('data', data => console.log(data))
   install.on('error', error => console.log(error))
